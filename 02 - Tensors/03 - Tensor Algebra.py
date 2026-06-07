99e069658e773b1b1286d6611b88d3245d6cc07e
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2026-06-07 19:21:46 +0200

    Fixes

--- 02 - Tensors/03 - Tensor Algebra.py
+++ 02 - Tensors/03 - Tensor Algebra.py
@@ -1,26 +1,25 @@
 import marimo
 
-__generated_with = "0.13.11"
+__generated_with = "0.23.5"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _():
     import marimo as mo
+
     return (mo,)
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     # Tensor Algebra
 
     [Sébastien Boisgérault], Mines Paris - PSL University
 
     [Sébastien Boisgérault]: mailto:Sebastien.Boisgerault@minesparis.psl.eu
-    """
-    )
+    """)
     return
 
 
@@ -28,13 +27,13 @@ def _(mo):
 def _():
     import torch
     from torch import tensor, tensordot, einsum
+
     return einsum, tensordot, torch
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## Tensor product & contraction
 
     | Tensor Operation      | Symbol    | Implementation                |
@@ -43,8 +42,7 @@ def _(mo):
     | Contraction           | $\cdot$   | `tensordot(..., ..., ndim=1)` |
     | Double Contraction    | $:$       | `tensordot(..., ..., ndim=2)` |
     | ...                   | ...       | ...                           |
-    """
-    )
+    """)
     return
 
 
@@ -68,8 +66,7 @@ def _(torch):
 
 @app.cell(hide_code=True)
 def _(R, S, a, b, mo, x, y):
-    mo.md(
-        rf"""
+    mo.md(rf"""
     | Variable Name      | Value                          | Rank     | Shape              |
     | ------------------ | ------------------------------ | -------- | ------------------ |
     | a                  | {a!r}                          | {a.ndim} | {tuple(a.shape)}   |
@@ -78,22 +75,19 @@ def _(R, S, a, b, mo, x, y):
     | y                  | {y!r}                          | {y.ndim} | {tuple(y.shape)}   |
     | R                  | {repr(R).replace("\n", " ")}   | {R.ndim} | {tuple(R.shape)}   |
     | S                  | {repr(S).replace("\n", " ")}   | {S.ndim} | {tuple(S.shape)}   |
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        rf"""
+    mo.md(rf"""
     ### Tensor product of scalars
 
     $$
-    [a] \otimes [b] = [a \times b] 
+    [a] \otimes [b] = [a \times b]
     $$
-    """
-    )
+    """)
     return
 
 
@@ -105,16 +99,14 @@ def _(a, b, tensordot):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Tensor product of 0d and 1d tensors
 
 
     $$
     [a] \otimes [x_i]_i = [x_i]_i \otimes [a] = [a \times x_i]_i
     $$
-    """
-    )
+    """)
     return
 
 
@@ -132,15 +124,13 @@ def _(a, tensordot, x):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
-    ### Tensor product of 1d tensors 
+    mo.md(r"""
+    ### Tensor product of 1d tensors
 
     $$
     [x_i]_i \otimes [y_j]_j = [x_i \times y_i]_{ij}
     $$
-    """
-    )
+    """)
     return
 
 
@@ -152,16 +142,14 @@ def _(tensordot, x, y):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        rf"""
-    ### Tensor contraction of 1d tensors 
+    mo.md(rf"""
+    ### Tensor contraction of 1d tensors
 
 
     $$
     [x_i]_i \cdot [y_j]_j = \left[\sum_k x_k y_k\right]
     $$
-    """
-    )
+    """)
     return
 
 
@@ -173,15 +161,13 @@ def _(tensordot, x, y):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
-    ### Tensor product of of 0d and 1d tensors 
+    mo.md(r"""
+    ### Tensor product of of 0d and 1d tensors
 
     $$
     [a] \otimes [R_{ij}]_{ij} = [R_{ij}]_{ij} \otimes [a] = [a \otimes R_{ij}]_{ij}
     $$
-    """
-    )
+    """)
     return
 
 
@@ -197,10 +183,9 @@ def _(R, a, tensordot):
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Tensor product of 1d and 2d tensors
 
     $$
@@ -210,8 +195,7 @@ def _(mo):
     $$
     [x_i]_i \otimes [R_{jk}]_{jk} = [x_i R_{jk}]_{ijk}
     $$
-    """
-    )
+    """)
     return
 
 
@@ -227,10 +211,9 @@ def _(R, torch, x):
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Tensor contraction of 1d and 2d tensor
 
     $$
@@ -240,8 +223,7 @@ def _(mo):
     $$
     [x_i]_i \cdot [R_{jk}]_{jk} = \left[\sum_l x_l R_{lk}\right]_{k}
     $$
-    """
-    )
+    """)
     return
 
 
@@ -259,15 +241,13 @@ def _(R, torch, x):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Tensor product of 2d tensors
 
     $$
     [R]_{ij} \otimes [S]_{kl} = [R_{ij} S_{kl}]_{ijkl}
     $$
-    """
-    )
+    """)
     return
 
 
@@ -279,15 +259,13 @@ def _(R, S, torch):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Tensor contraction of 2d tensors
 
     $$
     [R_{ij}]_{ij} \cdot [S_{kl}]_{kl} = \left[\sum_m R_{im} S_{ml} \right]_{il}
     $$
-    """
-    )
+    """)
     return
 
 
@@ -299,15 +277,13 @@ def _(R, S, torch):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Double contraction of 2d tensors
 
     $$
     [R_{ij}]_{ij} : [S_{kl}]_{kl} = \left[\sum_{m, n} R_{mn} S_{mn} \right]
     $$
-    """
-    )
+    """)
     return
 
 
@@ -319,63 +295,55 @@ def _(R, S, torch):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     /// warning | Shape compatibility
 
     The tensor product of two tensors $T_1$ and $T_2$ is defined unconditionally but their simple and double contraction are only defined when their shapes are compatible:
 
     | Operation          | Shape of $T_1$      | Shape of $T_2$      | Compatibility conditions | Shape of the result                  |
-    | ------------------ | ------------------- | ------------------- | ------------------------ | -------------------------------------- | 
+    | ------------------ | ------------------- | ------------------- | ------------------------ | -------------------------------------- |
     | $T_1 \otimes T_2$  | $(i_1, \dots, i_k)$ | $(j_1, \dots, j_l)$ |                          | $(i_1, \cdots, i_k, j_1, \dots, j_k)$  |
     | $T_1 \cdot T_2$    | $(i_1, \dots, i_k)$ | $(j_1, \dots, j_l)$ | $i_k=j_1$                | $(i_1, \cdots, i_{k-1}, j_2, \dots, j_k)$  |
     | $T_1 : T_2$        | $(i_1, \dots, i_k)$ | $(j_1, \dots, j_l)$ | $i_{k-1} = j_1$ and $i_k=j_2$ | $(i_1, \cdots, i_{k-2}, j_3, \dots, j_k)$      |
 
     ///
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     /// note | What about the `@` operator?
 
-    `T_1 @ T_2` is equivalent to `tensordot(T_1, T_2)` when `T_1` and `T_2` are 1d or 2d tensors.
+    `T_1 @ T_2` is equivalent to `tensordot(T_1, T_2, dim=1)` when `T_1` and `T_2` are 1d or 2d tensors.
 
-    For higher ranks, extra dimensions are interpreted as batch dimensions and the behavior is therefor different; refer to [torch.matmul] for more details.
+    For higher ranks, extra dimensions are interpreted as batch dimensions and the behavior is therefore different; refer to [torch.matmul] for more details.
 
     [torch.matmul]: https://docs.pytorch.org/docs/main/generated/torch.matmul.html
 
     ///
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ## Einstein Summation
 
     Similar to `tensordot`, but even more general.
-    """
-    )
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     $$
     [x_i]_i \to [x_i]_i
     $$
-    """
-    )
+    """)
     return
 
 
@@ -387,13 +355,11 @@ def _(einsum, x):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     $$
     [x_i]_i \to \left[\sum_i x_i\right]
     $$
-    """
-    )
+    """)
     return
 
 
@@ -405,15 +371,13 @@ def _(einsum, x):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Scalar product
 
     $$
     x \otimes y = [x_i y_j]_{ij}
     $$
-    """
-    )
+    """)
     return
 
 
@@ -425,13 +389,11 @@ def _(einsum, x, y):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     $$
     x \cdot y = \left[ \sum_k x_k y_k\right]
     $$
-    """
-    )
+    """)
     return
 
 
@@ -443,15 +405,13 @@ def _(einsum, x, y):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Transposition
 
     $$
-    [R_{ij}]_{ij} \to [R_{ji}]_{ij} 
+    [R_{ij}]_{ij} \to [R_{ji}]_{ij}
     $$
-    """
-    )
+    """)
     return
 
 
@@ -463,15 +423,13 @@ def _(R, einsum):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Trace
 
     $$
     [R_{ij}]_{ij} \to \left[\sum_{k} R_{kk} \right]
     $$
-    """
-    )
+    """)
     return
 
 
@@ -483,13 +441,11 @@ def _(R, einsum):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     $$
     R \otimes S = [R_{ij}S_{kl}]_{ijkl}
     $$
-    """
-    )
+    """)
     return
 
 
@@ -501,14 +457,12 @@ def _(R, S, torch):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     ### Matrix Product
     $$
     R \cdot S = \left[ \sum_m R_{im} S_{ml}\right]_{il}
     $$
-    """
-    )
+    """)
     return
 
 
@@ -520,13 +474,11 @@ def _(R, S, torch):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(
-        r"""
+    mo.md(r"""
     $$
     R : S = \left[ \sum_{m, n} R_{mn} S_{mn}\right]
     $$
-    """
-    )
+    """)
     return
 
 
@@ -538,13 +490,17 @@ def _(R, S, torch):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""### Batched Matrix Product""")
+    mo.md(r"""
+    ### Batched Matrix Product
+    """)
     return
 
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""Consider (2d) batches of matrices of size $(4,5)$ and $(5,6)$:""")
+    mo.md(r"""
+    Consider (2d) batches of matrices of size $(4,5)$ and $(5,6)$:
+    """)
     return
 
 
@@ -557,7 +513,9 @@ def _(torch):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""We can compute the batches of products with:""")
+    mo.md(r"""
+    We can compute the batches of products with:
+    """)
     return
 
 
@@ -569,7 +527,9 @@ def _(A, B, torch):
 
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""The ellipsis `...` provide a convenient shortcut for the batched dimensions.""")
+    mo.md(r"""
+    The ellipsis `...` provide a convenient shortcut for the batched dimensions.
+    """)
     return
 
 
